0ad66d7f06130e9abc18262afcbb61a6e94d70fe
Jingyuan_Li <jl11435jl11435@gmail.com>
2023-02-21 23:27:47 -0500

    Projectile Update

--- TA_OnClass/Assets/MyAssest/Shield/Script/SpawnMazzle.cs
+++ TA_OnClass/Assets/MyAssest/Shield/Script/SpawnMazzle.cs
@@ -5,10 +5,42 @@ using UnityEngine;
 public class SpawnMazzle : MonoBehaviour
 {
     [SerializeField] private GameObject _MazzlePrefab;
+
+    [Header("Projectile Parameter")]
+    [SerializeField] private float _projectileSpeed;
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Transform _projectileParent;
+    private ParticleSystem _ProjectileParticle;
+    private GameObject _Projectile;
+
+    //To count projectile velocity's direction
     [SerializeField] private Transform VFXTransform;
-    public void SpawnMazzleVFX()
+    [SerializeField] private Transform characterTrans;
+    [SerializeField] private Transform shieldTrans;
+
+    private void Update()
     {
-        GameObject vfxObject = Instantiate(_MazzlePrefab, VFXTransform);
-        Destroy(vfxObject,2f);
+        if (_Projectile != null)
+        {
+
+        }
     }
+
+    public void SpawnVFX()
+    {
+        //Spawn mazzle
+        GameObject mazzleObject = Instantiate(_MazzlePrefab, VFXTransform);
+        Destroy(mazzleObject,2f);
+
+        //Spawn projectile
+        _Projectile = Instantiate(_projectilePrefab, VFXTransform.position, VFXTransform.rotation, _projectileParent);
+        _ProjectileParticle = _Projectile.GetComponentsInChildren<ParticleSystem>()[0];
+        _ProjectileParticle.Play(true);
+        Vector3 projectileDirect = new Vector3(characterTrans.position.x - shieldTrans.position.x, VFXTransform.position.y - shieldTrans.position.y, characterTrans.position.z - shieldTrans.position.z);
+        //projectileObject.GetComponent<Rigidbody>().AddForce(projectileDirect.normalized * _projectileSpeed);
+        _Projectile.transform.position +=projectileDirect.normalized * _projectileSpeed;
+        Destroy(_Projectile, 1f);
+    }
+
+
 }
