38eb69526723d3b09c7c5e5a9f8de01994e31c4e
Jingyuan_Li <jl11435jl11435@gmail.com>
2023-02-25 17:59:37 -0500

    Shield Update

--- TA_OnClass/Assets/MyAssest/Shield/Script/SpawnRipple.cs
+++ TA_OnClass/Assets/MyAssest/Shield/Script/SpawnRipple.cs
@@ -6,20 +6,24 @@ using UnityEngine.VFX;
 public class SpawnRipple : MonoBehaviour
 {
     public GameObject shieldRipples;
+    public Transform parentTrans;
 
     private VisualEffect shieldRipplesVFX;
+
+
 
     private void OnCollisionEnter(Collision co)
     {
         
         if (co.gameObject.tag == "Bullet")
         {
-            Debug.Log("Enter");
-            var ripples = Instantiate(shieldRipples, transform) as GameObject;
+            //Debug.Log("Enter");
+            var ripples = Instantiate(shieldRipples, parentTrans) as GameObject;
             shieldRipplesVFX = ripples.GetComponent<VisualEffect>();
             shieldRipplesVFX.SetVector3("SphereCenter", co.contacts[0].point);
 
-            Destroy(ripples, 1);
+            Destroy(ripples, 0.2f);
+            Destroy(co.gameObject);
         }
     }
 }
